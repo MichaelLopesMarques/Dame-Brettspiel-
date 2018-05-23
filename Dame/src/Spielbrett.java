b8f94a63bf14b7d88fad6d300333b2aabff11a10
$5JUI00-S4J7VPLITD76 <$5JUI00-S4J7VPLITD76@LAB23-26.stud.hshl.net>
2018-05-23 10:49:28 +0200

    mal schaun

--- Dame/src/Spielbrett.java
+++ Dame/src/Spielbrett.java
@@ -3,5 +3,6 @@ public class Spielbrett {
 
 	public static void main(String[] args) {
 	      System.out.println("Hier soll mal ein Dame-Spiel entstehen!?");
+	      System.out.println("Jezuz ist der Beste!");
 	   }
 }
