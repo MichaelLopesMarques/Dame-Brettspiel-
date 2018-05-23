1e9e04e89bdfab6b81e396ac363e63b003ac7cfe
henri <henri@192.168.115.36>
2018-05-23 10:41:19 +0200

    Test 3

--- Dame/src/Spielbrett.java
+++ Dame/src/Spielbrett.java
@@ -2,6 +2,6 @@
 public class Spielbrett {
 
 	public static void main(String[] args) {
-	      System.out.println("Hi");
+	      System.out.println("Hier soll mal ein Dame-Spiel entstehen!?");
 	   }
 }
