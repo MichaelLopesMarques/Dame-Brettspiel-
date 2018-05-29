7c585b273d5ced1ceabbcb960c75e1831040d52b
Henri Katzenberg <henri.katzenberg@yahoo.de>
2018-05-29 23:24:24 +0200

    Menu Versuche

--- Dame/src/Spielbrett.java
+++ Dame/src/Spielbrett.java
@@ -1,19 +1,40 @@
-import java.awt.BorderLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.*;
 
 public class Spielbrett extends JFrame {
     
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public JButton single;
+	private JButton multi;
+	private JButton tut;
+	private JButton end;
+	
     public Spielbrett() {
     	super();
     	//pack();
     	JFrame frame = new JFrame ("Dame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,800);
+        frame.setSize(1280,720);
         frame.setResizable(false);
         frame.setVisible(true);
         
         Welt welt = new Welt();
+        
+        
+        single = new JButton("Singleplayer");
+        single.setBounds(80,20,600,100);
+        add(single);
+        
+    }
+    public void menu() {
+    	JMenuBar bar = new JMenuBar();
+    	JMenu menu = new JMenu("Datei");
+    	
+    	bar.add(menu);
+    	setJMenuBar(bar);
     }
 }
